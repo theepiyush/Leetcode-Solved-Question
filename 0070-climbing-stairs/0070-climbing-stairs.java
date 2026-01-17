c629d6322df63d8138eea5b40f8548b6bc804600
The_piyush <166884806+theepiyush@users.noreply.github.com>
2026-01-17 19:51:39 +0530

    Time: 0 ms (100%), Space: 42.2 MB (22.06%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,19 @@
+class Solution {
+    public static int climbStairs(int n) {
+        if(n<=3)
+        {
+            return n;
+        }
+        int a=3,b=2;
+        for(int i=0;i<n-3;i++)
+        {
+                a=a+b;
+                b=a-b;
+        }
+        return a;
+    }
+    public static void main(String[]args)
+    {
+        System.out.println(climbStairs(3));
+    }
+}
