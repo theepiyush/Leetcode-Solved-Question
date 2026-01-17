bc078f2b74107415863b7b91ca371ae202566a56
The_piyush <166884806+theepiyush@users.noreply.github.com>
2026-01-17 20:03:01 +0530

    Time: 1 ms (90.61%), Space: 42.8 MB (18.38%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -0,0 +1,29 @@
+class Solution {
+    public static int divide(int dividend, int divisor) {
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+        boolean negative = (dividend < 0) ^ (divisor < 0);
+        long a = Math.abs((long) dividend);
+        long b = Math.abs((long) divisor);
+        long result = 0;
+        while (a >= b) {
+            long temp = b, multiple = 1;
+            while (a >= (temp << 1)) {
+                temp <<= 1;
+                multiple <<= 1;
+            }
+            a -= temp;
+            result += multiple;
+        }
+        if (negative) result = -result;
+        if (result > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+        if (result < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+
+        return (int) result;
+    }
+    public static void main(String []args)
+    {
+        System.out.println(divide(10,3));
+    }
+}
