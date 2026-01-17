9b679b13c0633024ade4baefcf83fb3ce3ab3a35
The_piyush <166884806+theepiyush@users.noreply.github.com>
2026-01-17 19:55:40 +0530

    Time: 0 ms (100%), Space: 48.5 MB (26.32%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int start = 0;
+        int end = s.length - 1;
+
+        while (start < end) {
+            char temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        char[] s = {'h','e','l','l','o'};
+        sol.reverseString(s);
+        System.out.println(java.util.Arrays.toString(s)); 
+        
+    }
+}
