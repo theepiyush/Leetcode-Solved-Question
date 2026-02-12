4dbec6b62c74d7daf389e4483702c25e0c9dab13
Piyush Kumar Singh <166884806+theepiyush@users.noreply.github.com>
2026-02-12 21:02:53 +0530

    Time: 26 ms (94.53%), Space: 72.9 MB (95.98%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minSetSize(int[] arr) {
+        int n=arr.length;
+        int max=Integer.MIN_VALUE;
+       for(int e : arr) {
+    max = Math.max(max, e);
+}
+    int[] freq = new int[max + 1];
+        for(int e:arr)
+        {
+            freq[e]++;
+        }
+        Arrays.sort(freq);
+       int i = freq.length - 1;
+        int count=0;
+        while(n>arr.length/2)
+        {
+            n-=freq[i];
+            i--;
+            count++;
+
+        }
+       return count;
+        
+    }
+}
